bb9e008cf271eade69991019b340104adbcb1d32
abagri <ashish.bagri@ymail.com>
2023-07-12 00:20:29 +0800

    who owns the criterian

--- src/functional_programming/Car.java
+++ src/functional_programming/Car.java
@@ -1,2 +1,89 @@
-package functional_programming;public class Car {
+package functional_programming;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+public class Car {
+    private final int gasLevel;
+    private final String color;
+    private final List<String> passengers;
+    private final List<String> truckContents;
+
+    private Car(int gasLevel, String color, List<String> passengers, List<String> truckContents) {
+        this.gasLevel = gasLevel;
+        this.color = color;
+        this.passengers = passengers;
+        this.truckContents = truckContents;
+    }
+
+    public static Car withGasColorPassengers(int gas, String color, String... passengers){
+        List<String> p = Collections.unmodifiableList(Arrays.asList(passengers));
+        Car self = new Car(gas, color, p, null);
+        return self;
+    }
+
+    public static Car withGasColorPassengersAndTrunk(int gas, String  color, String... passengers){
+        List<String> p = Collections.unmodifiableList(Arrays.asList(passengers));
+        Car self = new Car(gas, color, p, Arrays.asList("jack","wrench","spare wheel"));
+        return self;
+    }
+
+    public int getGasLevel() {
+        return gasLevel;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public List<String> getPassengers() {
+        return passengers;
+    }
+
+    public List<String> getTruckContents() {
+        return truckContents;
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "gasLevel=" + gasLevel +
+                ", color='" + color + '\'' +
+                ", passengers=" + passengers +
+                (truckContents!=null ? ", truckContents=" + truckContents : " no trunk") +
+                '}';
+    }
+
+    static class RedCarCriterion implements CarCriterion {
+
+        @Override
+        public boolean test(Car c) {
+            return c.getColor().equalsIgnoreCase("Red");
+        }
+    }
+
+    static class GasLevelCarCriterion implements  CarCriterion {
+
+        private final int threshold;
+
+        public GasLevelCarCriterion(int threshold) {
+            this.threshold = threshold;
+        }
+
+        @Override
+        public boolean test(Car c) {
+            return c.getGasLevel() >= threshold;
+        }
+    }
+
+    static class PassengerCountOrder implements Comparator<Car> {
+
+        @Override
+        public int compare(Car o1, Car o2) {
+            return o1.getPassengers().size() -  o2.getPassengers().size();
+        }
+    }
+
 }
--- src/functional_programming/CarScratch.java
+++ src/functional_programming/CarScratch.java
@@ -1,2 +1,45 @@
-package functional_programming;public class CarScratch {
+package functional_programming;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
+interface CarCriterion {
+    boolean test(Car c);
 }
+
+public class CarScratch {
+
+    public static void showAll(List<Car> cars){
+        for(Car car : cars){
+            System.out.println(car);
+        }
+        System.out.println("------------------------------");
+    }
+
+    public static List<Car> getCarsByCriterion(Iterable<Car> in, CarCriterion carCriterion){
+        List<Car> output = new ArrayList<>();
+        for(Car c : in){
+            if(carCriterion.test(c))
+                output.add(c);
+        }
+        return output;
+    }
+
+    public static void main(String[] args) {
+        List<Car> cars = Arrays.asList(
+                Car.withGasColorPassengers(6, "Red", "Jim", "Sheila"),
+                Car.withGasColorPassengers(3, "Octarine", "Rincewind", "Ridcully"),
+                Car.withGasColorPassengers(9, "Black", "Weatherwax", "Margrat"),
+                Car.withGasColorPassengers(7, "Green", "Valentine", "Gilian","Anne", "Dr. Mahmoud"),
+                Car.withGasColorPassengers(6, "Red","Ender", "Hyrum", "Locke","Bonzo")
+        );
+        showAll(cars);
+        showAll(getCarsByCriterion(cars, new Car.RedCarCriterion()));
+        showAll(getCarsByCriterion(cars, new Car.GasLevelCarCriterion(6)));
+        cars.sort(new Car.PassengerCountOrder());
+        showAll(cars);
+    }
+}
+
